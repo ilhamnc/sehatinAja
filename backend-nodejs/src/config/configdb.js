5f12ed5e5665bbbac7f4b36c691cf9f1ae9baf9e
Ilham Nur Cahyo <101688850+ilhamnc@users.noreply.github.com>
2023-12-17 19:15:49 +0700

    Update configdb.js

--- backend-nodejs/src/config/configdb.js
+++ backend-nodejs/src/config/configdb.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: '34.101.253.45',
+  host: '34.101.103.107',
   user: 'root',
-  password: 'authentic1009',
+  password: 'sehatin1009',
   database: 'sehatin-db',
   port: '3306',
 });
