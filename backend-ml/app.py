5956e43dd61baeb84140a317cc66057d48225da2
Ilham Nur Cahyo <101688850+ilhamnc@users.noreply.github.com>
2023-12-20 22:51:34 +0700

    Update app.py

--- backend-ml/app.py
+++ backend-ml/app.py
@@ -2,14 +2,16 @@ import numpy as np
 import os
 from flask import Flask, request, jsonify
 import pickle
+from flask_cors import CORS
 
-app = Flask(__name__) # Inisialisasi aplikasi Flask
-model = pickle.load(open('model.pkl', 'rb')) # Memuat model yang telah dilatih
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Menerima input dari JSON
         data = request.get_json()
         Age = float(data['age'])
         Weight = float(data['weight'])
@@ -19,19 +21,14 @@ def predict():
         Calories = generate * 0.666
         Protein = generate * 0.333
         
-        # Membuat array input untuk prediksi
         final_features = np.array([[Calories, Protein]])
         
-        # Melakukan prediksi
         prediction = model.predict(final_features)
         
-        # Output prediksi dalam bentuk JSON
-        output = {'prediction': prediction[0], 'status_code': 200}
-
-        return jsonify(output), 200 # Mengembalikan prediksi dalam bentuk JSON dengan status 200
+        return jsonify({'error': False, 'message': 'success', 'predict' : prediction[0]}), 200
 
     except Exception as e:
-        return jsonify({'error': 'Terjadi kesalahan', 'status_code': 500}), 500 # Mengembalikan pesan error dengan status 500 jika terjadi kesalahan
+        return jsonify({'error': True, 'message': 'Terjadi kesalahan'}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080))) # Menjalankan aplikasi dengan port yang sesuai
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
